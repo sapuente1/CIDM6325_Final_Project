5ee7c80ed516d4857748e25812c4d2258b7d3073
Jeffry Babb <ahuimanu@gmail.com>
2025-10-10 18:41:13 -0500

    updated for django video 3

--- blog_site/blog/urls.py
+++ blog_site/blog/urls.py
@@ -4,9 +4,9 @@ from . import views
 app_name = "blog"
 
 urlpatterns = [
-    path("", views.post_list, name="post_list"),
-    path("post/new/", views.post_create, name="post_create"),    
-    path("post/<slug:slug>/", views.post_detail, name="post_detail"),
-    path("post/<slug:slug>/edit/", views.post_update, name="post_update"),
-    path("post/<slug:slug>/delete/", views.post_delete, name="post_delete"),
+    path("", views.PostListView.as_view(), name="post_list"),
+    path("post/new/", views.PostCreateView.as_view(), name="post_create"),
+    path("post/<slug:slug>/edit/", views.PostUpdateView.as_view(), name="post_update"),
+    path("post/<slug:slug>/delete/", views.PostDeleteView.as_view(), name="post_delete"),
+    path("post/<slug:slug>/", views.PostDetailView.as_view(), name="post_detail"),
 ]
