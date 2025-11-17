3b92e8d8b6403935f35f23ec25b283ae156ab09b
Jeffry Babb <ahuimanu@gmail.com>
2025-11-16 21:48:58 -0600

    feat(core): add canonical links and sitemap for SEO — Refs #63

--- travelmathlite/core/urls.py
+++ travelmathlite/core/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from django.contrib.sitemaps.views import sitemap
 from django.urls import include, path
+from django.views.generic import TemplateView
+
+from .sitemaps import StaticViewSitemap
+
+sitemaps = {
+    "static": StaticViewSitemap,
+}
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -27,4 +35,7 @@ urlpatterns = [
     path("accounts/", include("apps.accounts.urls", namespace="accounts")),
     path("trips/", include("apps.trips.urls", namespace="trips")),
     path("search/", include("apps.search.urls", namespace="search")),
+    # SEO
+    path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="django.contrib.sitemaps.views.sitemap"),
+    path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain"), name="robots"),
 ]
